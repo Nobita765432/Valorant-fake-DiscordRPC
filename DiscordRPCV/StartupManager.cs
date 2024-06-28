688fe1d5e8b70441b2de6daeb4b6e8d1cbe24003
Gaming Nove <104850985+Nobita765432@users.noreply.github.com>
2024-06-28 09:37:21 +0600

    Add files via upload

---
+++ DiscordRPCV/StartupManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System;
+
+public static class StartupManager
+{
+    public static void SetStartup()
+    {
+        string appName = "ValorantDRPC"; // Replace with your application name
+        string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+        {
+            key.SetValue(appName, appPath);
+        }
+
+        Console.WriteLine($"{appName} set to run on startup.");
+    }
+
+    public static bool IsStartupSet()
+    {
+        string appName = "ValorantDRPC"; // Replace with your application name
+
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+        {
+            return key.GetValue(appName) != null;
+        }
+    }
+}
